06d02fa71092f7eea7b59dc5952df6bca2d6ae38
a-johanes <antoniusjohanes99@gmail.com>
2024-01-15 01:05:45 +0800

    init

---
+++ dto.py
@@ -0,0 +1,137 @@
+from dataclasses import dataclass
+from typing import List, Optional, Dict, Any
+import datetime
+from enum import Enum
+
+class InOutCode(Enum):
+    Income = 0
+    Expenses = 1
+
+@dataclass
+class Category:
+    id: str
+    name: str
+    in_out_code: InOutCode
+    sub_category: Optional[List['Category']] = None
+
+    @staticmethod
+    def from_money_book(obj: Dict[str, Any], in_out_code: InOutCode) -> 'Category':
+        id = obj.get('mcid', obj.get('mcscid', ''))
+        name = obj.get('mcname', obj.get('mcscname', ''))
+        sub_category = [ Category.from_money_book(cat, in_out_code) for cat in obj.get('mcsc',[])] or None
+        return Category(id, name, in_out_code, sub_category)
+
+    def to_dict(self) -> Dict[str,Any]:
+        d = {
+            "id": self.id,
+            "name": self.name,
+            "in_out_code": self.in_out_code.name
+        }
+
+        if self.sub_category is not None:
+            d["sub_category"] = [c.to_dict() for c in self.sub_category]
+
+        return d
+    
+@dataclass
+class Asset:
+    id: str
+    name: str
+    money: float
+
+    @staticmethod
+    def from_money_book(obj: Dict[str, Any]) -> 'Asset':
+        id = obj.get('assetId', '')
+        name = obj.get('assetName','')
+        money = obj.get('assetMoney','')
+        money = float(money)
+        return Asset(id, name, money)
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {
+            "id": self.id,
+            "name": self.name,
+            "money": self.money,
+        }
+
+@dataclass
+class AssetGroup:
+    id: str = ""
+    name: str = ""
+    money: float = 0
+    children: Optional[List['Asset']] = None
+
+    @staticmethod
+    def from_money_book( obj: Dict[str, Any]) -> 'AssetGroup':
+        id = obj.get('assetGroupId', '')
+        name = obj.get('assetName', '')
+        money = obj.get('assetMoney', '')
+        money = float(money)
+
+        children = [ Asset.from_money_book(asset) for asset in obj.get('children',[])]
+        return AssetGroup(id, name, money, children)
+    
+    def to_dict(self) -> Dict[str, Any]:
+        return {
+            "id": self.id,
+            "name" : self.name,
+            "money": self.money,
+            "children": [c.to_dict() for c in self.children]
+        }
+    
+
+
+@dataclass
+class CreateInOutTransactionRequest:
+    in_out_code: InOutCode
+    asset: Asset
+    category: Category 
+    date: datetime.date = datetime.datetime.now()
+    money: Optional[float] = None
+    note: Optional[str] = None
+    description: Optional[str] = None
+    sub_category: Optional[Category] = None
+
+    def __post_init__(self):
+        if self.in_out_code != self.category.in_out_code:
+            raise ValueError('category in out code has to be the same with transaction in out code')
+        
+        if self.sub_category is not None:
+            if self.category.sub_category is None:
+                raise ValueError("the category doesn't has any sub category")
+
+            if self.sub_category not in self.category.sub_category:
+                raise ValueError('sub category is not part of the given category')
+
+    def to_dict(self) -> Dict[str, Any]:
+        d = {
+            'mbDate': self.date.strftime('%Y-%m-%dT%H:%M:%S'),
+            'inOutCode' : self.in_out_code.value,
+            'assetId': self.asset.id,
+            'mcid': self.category.id,
+        }
+
+        if self.sub_category is not None:
+            d['mcscid'] = self.sub_category.id
+
+        field_names = {
+            'money': 'mbCash',
+            'note': 'mbContent',
+            'description': 'mbDetailContent'
+        }
+
+        for field_name, request_field_name in field_names.items():
+            field_value = self.__dict__[field_name]
+            
+            if field_value is not None:
+                d[request_field_name] = field_value
+
+        return d
+
+
+if __name__ == '__main__':
+    asset = Asset("9a423682-a294-46a4-b71a-7b1feb6da4fc",'name',110)
+    sub_category = Category('49abda9d-53a7-4a49-9006-9c1833f859f5','rent', InOutCode.Expenses)
+    category = Category('11', 'household', InOutCode.Expenses, sub_category=[sub_category])
+    r = CreateInOutTransactionRequest(InOutCode.Expenses, asset, category, sub_category=sub_category)
+    print(r.to_dict())
---
+++ simply_go.py
@@ -0,0 +1,246 @@
+
+import datetime
+from enum import Enum
+from dataclasses import dataclass
+import re
+from typing import List, Tuple
+import PyPDF2
+
+from dto import *
+from finance_manager import *
+
+
+class TransportType(Enum):
+    MRT = 0
+    BUS = 1
+    MIXED = 2
+
+
+@dataclass
+class Transaction:
+    time: datetime.time
+    from_destination: str
+    to_destination: str
+    fare: float
+    transport: 'TransportType'
+
+
+@dataclass
+class Trip:
+    date: datetime.date
+    from_destination: str
+    to_destination: str
+    fare: float
+    transactions: List['Transaction']
+
+    def __str__(self) -> str:
+        transaction_str = [str(transaction)
+                           for transaction in self.transactions]
+        transaction_lines = '\n'.join(transaction_str)
+
+        return f'Trip(date={self.date}, from={self.from_destination}, to={self.to_destination}, fare={self.fare}, transport={self.get_transport_type()}, transactions=\n{transaction_lines})'
+
+    def get_transport_type(self) -> 'TransportType':
+        transportTypes = {
+            transaction.transport for transaction in self.transactions}
+
+        match len(transportTypes):
+            case 0:
+                return None
+            case 1:
+                return transportTypes.pop()
+            case _:
+                return TransportType.MIXED
+
+    def to_request(self, asset_groups: List[AssetGroup], categories: Dict[InOutCode, List[Category]]) -> CreateInOutTransactionRequest:
+        in_out_code = InOutCode.Expenses
+        asset_group = next(
+            (group for group in asset_groups if group.id == '1'), None)
+        if asset_group is None:
+            return
+        # print(asset_group)
+        asset = next((asset for asset in asset_group.children if asset.id ==
+                     '05c64c05-8fa5-4b8d-a33c-0ab1a662fc65'), None)
+        if asset is None:
+            return
+        # print(asset)
+        expense_categories = categories[in_out_code]
+        category = next(
+            (cat for cat in expense_categories if cat.id == '9'), None)
+        if category is None:
+            return
+        # print(category)
+        last_transaction = self.transactions[-1]
+
+        time = datetime.datetime.combine(self.date, last_transaction.time)
+
+        sub_category = None
+        match self.get_transport_type():
+            case TransportType.MRT:
+                sub_category = next((cat for cat in category.sub_category if cat.id ==
+                                    '0fcb0e69-1b4e-4362-8d08-17d741deef39'), None)
+            case TransportType.BUS:
+                sub_category = next(
+                    (cat for cat in category.sub_category if cat.id == '26'), None)
+            case TransportType.MIXED:
+                next((cat for cat in category.sub_category if cat.id ==
+                                    '8b3d8e7f-7845-40fe-844f-11855077ded6'), None)
+
+        request = CreateInOutTransactionRequest(
+            in_out_code,
+            asset,
+            category,
+            time,
+            self.fare,
+            f'{self.from_destination} - {self.to_destination}',
+            sub_category=sub_category,
+        )
+
+        return request
+
+
+class SimplyGo:
+
+    @staticmethod
+    def parse_pdf(path: str) -> List['Trip']:
+        pdf_text = SimplyGo.extract_pdf(path)
+        # print(*pdf_text,sep='\n')
+        return SimplyGo.parse_trip_data(pdf_text)
+
+    def extract_pdf(path: str) -> List[str]:
+        with open(path, 'rb') as f:
+            pdf_reader = PyPDF2.PdfReader(f)
+
+            result = []
+
+            for page in pdf_reader.pages:
+                text_list = page.extract_text(space_width=1.0).splitlines()
+                # print(text_list)
+                text_list = [line for line in text_list if line != " "]
+
+                result += text_list
+
+            return result
+
+    trip_start_regex = re.compile(
+        "^(\\w{3}, \\d{2}/\\d{2}/\\d{4})(\s?)(.*) - (((.*) \\$(\\d+\\.\\d+))|(.*))$")
+    fare_regex = re.compile("^(.*)\\$([0-9.]+)$")
+    tansaction_start_regex = re.compile(
+        "^(\\d{2}:\\d{2} [AP][M])(.*) - ((.*) (\\$([0-9.]+))|(.*))$")
+
+    date_format_pattern = "%a, %d/%m/%Y"
+    time_format_pattern = "%I:%M %p"
+
+    @staticmethod
+    def parse_trip_data(data: List[str]) -> List['Trip']:
+        trips = []
+        curr_trip: Trip = None
+        curr_transaction: Transaction = None
+        need_transaction_detail: bool = False
+
+        for line in data:
+            trimmed_line = line.strip()
+
+            if trimmed_line.startswith('POSTED'):
+                continue
+
+            trip_start_match = SimplyGo.trip_start_regex.findall(trimmed_line)
+            if len(trip_start_match) > 0:
+                if curr_trip is not None:
+                    trips.append(curr_trip)
+
+                value = trip_start_match[0]
+                curr_trip = Trip(
+                    date=datetime.datetime.strptime(
+                        value[0], SimplyGo.date_format_pattern).date(),
+                    from_destination=value[2],
+                    to_destination=value[5] if value[6] != "" else value[7],
+                    fare=float(value[6]) if value[6] != "" else 0,
+                    transactions=[]
+                )
+
+                continue
+
+            if trimmed_line.startswith("[Posting"):
+                fare_match = SimplyGo.fare_regex.findall(trimmed_line)
+                if len(fare_match) > 0:
+                    curr_trip.fare = float(fare_match[0][1])
+                else:
+                    raise ValueError("Wrong format, posting doesn't have fare")
+
+                continue
+
+            transaction_start_match = SimplyGo.tansaction_start_regex.findall(
+                trimmed_line)
+            if len(transaction_start_match) > 0:
+                value = transaction_start_match[0]
+
+                fare = value[5]
+                # determined full description based on if it contains the fare
+                need_transaction_detail = fare == ""
+
+                to_destination: str = value[3] or value[6]
+                match = re.findall("\\(\\d+\\)", to_destination)
+
+                curr_transaction = Transaction(
+                    time=datetime.datetime.strptime(
+                        value[0], SimplyGo.time_format_pattern).time(),
+                    from_destination=value[1],
+                    to_destination=to_destination,
+                    fare=fare if not need_transaction_detail else 0,
+                    transport=TransportType.BUS if match else TransportType.MRT
+                )
+
+                if not need_transaction_detail:
+                    curr_trip.transactions.append(curr_transaction)
+
+                continue
+
+            if need_transaction_detail and not trimmed_line == "":
+                fare_match = SimplyGo.fare_regex.findall(trimmed_line)
+                if len(fare_match) > 0:
+                    # end of transaction, found fare
+                    value = fare_match[0]
+                    route_detail = value[0].strip()
+
+                    curr_transaction.to_destination += f' {route_detail}'
+                    curr_transaction.fare = float(value[1])
+
+                    match = re.findall("\\(\\d+\\)", route_detail)
+                    curr_transaction.transport = TransportType.BUS if match else TransportType.MRT
+
+                    need_transaction_detail = False
+                    curr_trip.transactions.append(curr_transaction)
+                else:
+                    raise ValueError("missing transaction detail")
+
+                continue
+
+        trips.append(curr_trip)
+        return trips
+
+
+if __name__ == '__main__':
+    m = FinanceManager("192.168.0.197:8888")
+    m.load_asset_data()
+    m.load_init_data()
+
+    # print(m.asset_groups)
+    # print(m.expense_categories)
+    # print(m.income_categories)
+
+    assets = m.asset_groups
+    categories = {
+        InOutCode.Expenses: m.expense_categories,
+        InOutCode.Income: m.income_categories,
+    }
+
+    trips = SimplyGo.parse_pdf('../doc/TL-SimplyGo-TransactionHistory-15-Jan-24-00-23-20.pdf')
+    # request_list:List[CreateInOutTransactionRequest] = []
+
+    for trip in trips[::-1]:
+        request = trip.to_request(assets, categories)
+        print(request.to_dict())
+        # request_list.append(request)
+
+        m.create_in_out_transaction(request)
