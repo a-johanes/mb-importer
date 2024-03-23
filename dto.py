9857bf43de74ffda4e0d1a280dc9a5810259d5ac
a-johanes <antoniusjohanes99@gmail.com>
2024-03-23 17:01:38 +0800

    Add create transfer request

--- dto.py
+++ dto.py
@@ -3,9 +3,11 @@ from typing import List, Optional, Dict, Any
 import datetime
 from enum import Enum
 
+
 class InOutCode(Enum):
     Income = 0
     Expenses = 1
+
 
 @dataclass
 class Category:
@@ -18,10 +20,10 @@ class Category:
     def from_money_book(obj: Dict[str, Any], in_out_code: InOutCode) -> 'Category':
         id = obj.get('mcid', obj.get('mcscid', ''))
         name = obj.get('mcname', obj.get('mcscname', ''))
-        sub_category = [ Category.from_money_book(cat, in_out_code) for cat in obj.get('mcsc',[])] or None
+        sub_category = [Category.from_money_book(cat, in_out_code) for cat in obj.get('mcsc', [])] or None
         return Category(id, name, in_out_code, sub_category)
 
-    def to_dict(self) -> Dict[str,Any]:
+    def to_dict(self) -> Dict[str, Any]:
         d = {
             "id": self.id,
             "name": self.name,
@@ -32,7 +34,8 @@ class Category:
             d["sub_category"] = [c.to_dict() for c in self.sub_category]
 
         return d
-    
+
+
 @dataclass
 class Asset:
     id: str
@@ -42,8 +45,8 @@ class Asset:
     @staticmethod
     def from_money_book(obj: Dict[str, Any]) -> 'Asset':
         id = obj.get('assetId', '')
-        name = obj.get('assetName','')
-        money = obj.get('assetMoney','')
+        name = obj.get('assetName', '')
+        money = obj.get('assetMoney', '')
         money = float(money)
         return Asset(id, name, money)
 
@@ -54,6 +57,7 @@ class Asset:
             "money": self.money,
         }
 
+
 @dataclass
 class AssetGroup:
     id: str = ""
@@ -62,30 +66,29 @@ class AssetGroup:
     children: Optional[List['Asset']] = None
 
     @staticmethod
-    def from_money_book( obj: Dict[str, Any]) -> 'AssetGroup':
+    def from_money_book(obj: Dict[str, Any]) -> 'AssetGroup':
         id = obj.get('assetGroupId', '')
         name = obj.get('assetName', '')
         money = obj.get('assetMoney', '')
         money = float(money)
 
-        children = [ Asset.from_money_book(asset) for asset in obj.get('children',[])]
+        children = [Asset.from_money_book(asset) for asset in obj.get('children', [])]
         return AssetGroup(id, name, money, children)
-    
+
     def to_dict(self) -> Dict[str, Any]:
         return {
             "id": self.id,
-            "name" : self.name,
+            "name": self.name,
             "money": self.money,
             "children": [c.to_dict() for c in self.children]
         }
-    
 
 
 @dataclass
 class CreateInOutTransactionRequest:
     in_out_code: InOutCode
     asset: Asset
-    category: Category 
+    category: Category
     date: datetime.date = datetime.datetime.now()
     money: Optional[float] = None
     note: Optional[str] = None
@@ -95,7 +98,7 @@ class CreateInOutTransactionRequest:
     def __post_init__(self):
         if self.in_out_code != self.category.in_out_code:
             raise ValueError('category in out code has to be the same with transaction in out code')
-        
+
         if self.sub_category is not None:
             if self.category.sub_category is None:
                 raise ValueError("the category doesn't has any sub category")
@@ -106,7 +109,7 @@ class CreateInOutTransactionRequest:
     def to_dict(self) -> Dict[str, Any]:
         d = {
             'mbDate': self.date.strftime('%Y-%m-%dT%H:%M:%S'),
-            'inOutCode' : self.in_out_code.value,
+            'inOutCode': self.in_out_code.value,
             'assetId': self.asset.id,
             'mcid': self.category.id,
         }
@@ -122,7 +125,38 @@ class CreateInOutTransactionRequest:
 
         for field_name, request_field_name in field_names.items():
             field_value = self.__dict__[field_name]
-            
+
+            if field_value is not None:
+                d[request_field_name] = field_value
+
+        return d
+
+
+@dataclass
+class CreateTransferTransactionRequest:
+    from_asset: Asset
+    to_asset: Asset
+    date: datetime.date = datetime.datetime.now()
+    money: Optional[float] = None
+    note: Optional[str] = None
+    description: Optional[str] = None
+
+    def to_dict(self) -> Dict[str, Any]:
+        d = {
+            'moveDate': self.date.strftime('%Y-%m-%dT%H:%M:%S'),
+            'toAssetId': self.to_asset.id,
+            'fromAssetId': self.from_asset.id,
+        }
+
+        field_names = {
+            'money': 'moveMoney',
+            'note': 'moneyContent',
+            'description': 'mbDetailContent'
+        }
+
+        for field_name, request_field_name in field_names.items():
+            field_value = self.__dict__[field_name]
+
             if field_value is not None:
                 d[request_field_name] = field_value
 
@@ -130,8 +164,9 @@ class CreateInOutTransactionRequest:
 
 
 if __name__ == '__main__':
-    asset = Asset("9a423682-a294-46a4-b71a-7b1feb6da4fc",'name',110)
-    sub_category = Category('49abda9d-53a7-4a49-9006-9c1833f859f5','rent', InOutCode.Expenses)
-    category = Category('11', 'household', InOutCode.Expenses, sub_category=[sub_category])
-    r = CreateInOutTransactionRequest(InOutCode.Expenses, asset, category, sub_category=sub_category)
+    asset = Asset("9a423682-a294-46a4-b71a-7b1feb6da4fc", 'name', 110)
+    # sub_category = Category('49abda9d-53a7-4a49-9006-9c1833f859f5', 'rent', InOutCode.Expenses)
+    # category = Category('11', 'household', InOutCode.Expenses, sub_category=[sub_category])
+    # r = CreateInOutTransactionRequest(InOutCode.Expenses, asset, category, sub_category=sub_category)
+    r = CreateTransferTransactionRequest(asset, asset, money=10)
     print(r.to_dict())
