d08286b1971156b95a1872b4fa573a95b5afd357
a-johanes <antoniusjohanes99@gmail.com>
2024-03-24 00:43:31 +0800

    Add create transfer transaction to finance manager

--- finance_manager.py
+++ finance_manager.py
@@ -10,43 +10,43 @@ from dto import *
 class FinanceManager:
     def __init__(self, url: str):
         self.base_url = url
-        self.income_categories  : List['Category']= []
-        self.expense_categories : List['Category'] = []
-        self.asset_groups : List['AssetGroup'] = []
+        self.income_categories: List['Category'] = []
+        self.expense_categories: List['Category'] = []
+        self.asset_groups: List['AssetGroup'] = []
 
     def get_remote_init_data(self):
         url = f"http://{self.base_url}/moneyBook/getInitData"
-        
+
         response = requests.get(url)
         data = cson.loads(response.content)
 
-        with open("./remote_all_data.json","w") as f:
+        with open("./remote_all_data.json", "w") as f:
             json.dump(data, f, indent=4)
 
     def get_remote_asset_data(self):
         url = f"http://{self.base_url}/moneyBook/getAssetData"
-        
+
         response = requests.get(url)
         data = cson.loads(response.content)
 
-        with open("./remote_asset_data.json","w") as f:
+        with open("./remote_asset_data.json", "w") as f:
             json.dump(data, f, indent=4)
-    
+
     def load_init_data(self):
         with open("./remote_all_data.json") as f:
             data = json.load(f)
 
         for cat in data['category_0']:
             self.income_categories.append(Category.from_money_book(cat, InOutCode.Income))
-        
+
         for cat in data['category_1']:
-            self.expense_categories.append(Category.from_money_book(cat,InOutCode.Expenses))
+            self.expense_categories.append(Category.from_money_book(cat, InOutCode.Expenses))
 
         income_category = [c.to_dict() for c in self.income_categories]
         expense_category = [c.to_dict() for c in self.expense_categories]
 
-        with open("./init_data.json","w") as f:
-            json.dump({"income_category": income_category,"expense_category": expense_category}, f, indent=4)
+        with open("./init_data.json", "w") as f:
+            json.dump({"income_category": income_category, "expense_category": expense_category}, f, indent=4)
 
     def load_asset_data(self):
         with open("./remote_asset_data.json") as f:
@@ -57,12 +57,18 @@ class FinanceManager:
 
         asset_group = [a.to_dict() for a in self.asset_groups]
 
-        with open("./asset_data.json","w") as f:
+        with open("./asset_data.json", "w") as f:
             json.dump(asset_group, f, indent=4)
 
     def create_in_out_transaction(self, request: CreateInOutTransactionRequest):
         url = f"http://{self.base_url}/moneyBook/create"
-        
+
+        resp = requests.post(url, data=request.to_dict())
+        print(resp)
+
+    def create_transfer_transaction(self, request: CreateTransferTransactionRequest):
+        url = f"http://{self.base_url}/moneyBook/moveAsset"
+
         resp = requests.post(url, data=request.to_dict())
         print(resp)
 
